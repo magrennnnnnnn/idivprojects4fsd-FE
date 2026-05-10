0b28e35156a235756a789102decb2285c6d006a8
magrennn <mmagrenturcu20@gmail.com>
2026-05-11 01:02:25 +0200

    fixed the routes

--- prolink-fe/src/App.jsx
+++ prolink-fe/src/App.jsx
@@ -3,19 +3,21 @@ import RegisterForm from "./components/RegisterForm.jsx";
 import LoginForm from "./components/LoginForm.jsx";
 import CreateProfileForm from "./components/CreateProfileForm.jsx";
 import ProfilePage from "./components/ProfilePage.jsx";
+import FeedPage from "./components/FeedPage.jsx";
+import PostHistoryPage from "./components/PostHistoryPage.jsx";
 import "./App.css";
 
 function App() {
   return (
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<RegisterForm />} />
-          <Route path="/login" element={<LoginForm />} />
-            <Route path="/feed" element={<FeedPage />} />
-            <Route path="/profile/create" element={<CreateProfileForm />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/profile/posts" element={<PostHistoryPage />} />
-        </Routes>
+          <Routes>
+              <Route path="/" element={<RegisterForm />} />
+              <Route path="/login" element={<LoginForm />} />
+              <Route path="/feed" element={<FeedPage />} />
+              <Route path="/profile/create" element={<CreateProfileForm />} />
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/profile/posts" element={<PostHistoryPage />} />
+          </Routes>
       </BrowserRouter>
   );
 }
